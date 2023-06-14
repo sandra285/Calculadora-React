cc5b80cd5b112b81937d4ecef4526d75aeab1b4d
sandra285 <sandrafabrizi87@gmail.com>
2023-06-13 21:23:37 -0300

    Se agregan logo y contenedor principal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className='App'>
+     <div className='freecodecamp-logo-contenedor'>
+      <img 
+        className='freecodecamp-logo'
+        src={freeCodeCampLogo} 
+        alt='Logo de freeCodeCamp' />
+     </div>
+     <div className='contenedor-calculadora'>
+
+     </div>
     </div>
   );
 }
